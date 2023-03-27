11583cea81498ce41acb805ea57b5566dacb12ab
胡政涛 <452580164@qq.com>
2023-03-27 10:58:37 +0800

    创建好mock数据

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { buildURL } from "./helper/utils";
-import { AxiosRequestConfig } from "./types";
+import { buildURL } from './helper/url'
+import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 
-function axios(config: AxiosRequestConfig):void {
+function axios(config: AxiosRequestConfig): void {
   processConfig(config)
   xhr(config)
 }
 
-function processConfig (config: AxiosRequestConfig){
+function processConfig(config: AxiosRequestConfig) {
   config.url = transformUrl(config)
 }
 
-function transformUrl(config: AxiosRequestConfig): string{
+function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
 }
